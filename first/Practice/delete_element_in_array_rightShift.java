ed4970a8061bebf13b4a588749b7f2868e2db47a
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2025-09-24 22:49:41 +0530

    deleting element of array at a position using right shift

---
+++ first/Practice/delete_element_in_array_rightShift.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class delete_element_in_array_rightShift {
+    public static void main(String[] args){
+        int arr[] = {10,20,30,40,50};
+        int pos = 2;
+        for(int i=pos;i>0;i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = 0;
+        for(int i=1;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
