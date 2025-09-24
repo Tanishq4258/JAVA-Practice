be3968fec8f4d439627ac05959668e6a12815870
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2025-09-24 23:28:50 +0530

    special char right triangle

--- first/Practice/RightTriangle_SymbolPattern.java
+++ first/Practice/RightTriangle_SymbolPattern.java
@@ -2,11 +2,10 @@ package Practice;
 
 public class RightTriangle_SymbolPattern {
         public static void main(String[] args) {
-            int n = 5;
-            char[] symbols = {'@', '*', '#'};
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j <= i; j++) {
-                    System.out.print(symbols[j % 3] + " ");
+            String str="!@#$";
+            for(int i = 0; i<str.length(); i++){
+                for(int j=0;j<=i;j++){
+                    System.out.print(str.charAt(j) + " ");
                 }
                 System.out.println();
             }
