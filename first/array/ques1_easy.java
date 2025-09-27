522ca8b0db08d587542802c6172075de403f15c1
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2025-09-27 14:03:50 +0530

    array easy questions

---
+++ first/array/ques1_easy.java
@@ -0,0 +1,10 @@
+package array;
+
+public class ques1_easy {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        for(int i=0;i<5;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
