23d1a09c9f815bb596118c036481c7323c36c160
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2025-10-03 22:00:07 +0530

    power function

---
+++ first/Practice/power.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class power {
+    public static void main(String[] args){
+        int num = -3, exp = 3;
+        System.out.println(powwer(num,exp));
+    }
+    private static double powwer(int num, int exp){
+        return Math.pow(num, exp);
+    }
+}
