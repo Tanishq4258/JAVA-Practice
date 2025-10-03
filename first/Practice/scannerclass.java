19b84d33a66949c4e96ee19b1f1e5855c8b62e2a
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2025-10-03 20:23:34 +0530

    learning scanner class

---
+++ first/Practice/scannerclass.java
@@ -0,0 +1,14 @@
+package Practice;
+import java.util.Scanner;
+
+
+public class scannerclass {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+        String nn= sc.next();
+        String nnn = sc.nextLine();
+        System.out.println(nn + nnn);
+    }
+}
